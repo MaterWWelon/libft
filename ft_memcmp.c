c72b389c53813911bf74e6607f6e337d5b934dbb
Mathieu BELLINI <mbellini@e2r1p12.clusters.42paris.fr>
2021-12-13 10:04:15 +0100

    13/12

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: mbellini <mbellini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 12:42:19 by mbellini          #+#    #+#             */
-/*   Updated: 2021/12/09 17:55:00 by mbellini         ###   ########.fr       */
+/*   Updated: 2021/12/13 10:03:00 by mbellini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,22 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t			i;
 	unsigned char	*ptr1;
 	unsigned char	*ptr2;
+	int				i;
 
 	ptr1 = (unsigned char *)s1;
 	ptr2 = (unsigned char *)s2;
 	i = 0;
-	while (n > 0)
+	if (n == 0)
+		return (0);
+	while (ptr1 + i != 0 && ptr2 + i != 0 && i < (int)n)
 	{
-		if (ptr1[i] < ptr2[i])
-			return (-(ptr2[i] - ptr1[i]) % 256);
-		if (ptr1[i] > ptr2[i])
-			return ((ptr1[i] - ptr2[i]) % 256);
+		if (ptr1[i] != ptr2[i])
+			return (ptr1[i] - ptr2[i]);
 		i++;
-		n--;
 	}
+	if (ptr1 + i == 0 || ptr2 + i == 0)
+		return (ptr1[i] - ptr2[i]);
 	return (0);
 }
